41c41d5dd336b2f2b47b0af36fdd177f9caa2831
trevikenya <trevikenya@gmail.com>
2023-10-21 15:07:12 -0700

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys, math
+    import sys
     result = 0
-    for i in sys.argv:
-        result += int(i)
-        print("{}".format(result))
+    for arg in sys.argv:
+        if arg != sys.argv[0]:
+            result += int(arg)
+    print(result)
