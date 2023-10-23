6378cc9f252420b46ae8590c108869eea0e4e8db
trevikenya <trevikenya@gmail.com>
2023-10-23 15:33:56 -0700

    my pythone-execeptions work

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        div = a / b
-    except (ZeroDivisionError):
-        div = None
+        answer = a / b
+    except (ZeroDivisionError, FloatingPointError):
+        answer = None
+    finally:
+        print("Inside result: {}".format(answer))
+    return answer
